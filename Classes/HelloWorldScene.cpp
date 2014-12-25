7696ac88333c29816d6ac81cba0aad29bdb35d38
tonitakekawa <tointakekawa@gmail.com>
2014-12-25 22:10:10 +0900

    コメントを削除

--- Classes/HelloWorldScene.cpp
+++ Classes/HelloWorldScene.cpp
@@ -26,6 +26,14 @@ bool HelloWorld::init()
 	node->setName("NODE");
 	this->addChild(node);
 
+	// 縦横比を調整する
+	float dispRatio = vsize.width / vsize.height;
+	float nodeRatio = node->getContentSize().width / node->getContentSize().height;
+	float vmag = vsize.width / node->getContentSize().width;
+	float hmag = vsize.height / node->getContentSize().height;
+	node->setScale(vmag);
+
+	// 終了ボタン押下時のアクション
 	auto touchQuit = [this, director](Ref* pSender, ui::Widget::TouchEventType type){
 		if (type == ui::Widget::TouchEventType::ENDED) {
 			director->end();
@@ -37,18 +45,6 @@ bool HelloWorld::init()
 
 	auto sbtn = (cocos2d::ui::Widget*)(this->getChildByName("NODE")->getChildByName("START"));
 	sbtn->addTouchEventListener(touchQuit);
-
-	/*
-    auto label = Label::createWithTTF("Hello World", "fonts/Marker Felt.ttf", 24);
-	label->setPosition(
-		Vec2(org.x + vsize.width / 2,
-		     org.y + vsize.height - label->getContentSize().height));
-    this->addChild(label, 1);
-
-    auto sprite = Sprite::create("HelloWorld.png");
-	sprite->setPosition(Vec2(vsize.width / 2 + org.x, vsize.height / 2 + org.y));
-    this->addChild(sprite, 0);
-    */
 
     return true;
 }
